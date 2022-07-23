6a8a8bee5023d2cd6d5cba76153a63b9089b5af0
nikolaChakarov <chakarovn1975@gmail.com>
2022-07-23 19:44:37 +0300

    home page

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <HomeContainer>
+            <h1>
+                Find you best <span>5</span> iTunes
+            </h1>
+            <Link to={"/form"}>...in the next page</Link>
+        </HomeContainer>
+    );
 };
+
+const HomeContainer = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    align-items: center;
+    justify-content: center;
+
+    h1 {
+        color: var(--color-main);
+        font-weight: 900;
+    }
+
+    a {
+        color: var(--color-gray);
+    }
+`;
 
 export default Home;
